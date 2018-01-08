365162d63112e16eee3a4c1a6e5cdce4f12d0681
yuzoaragaki <yuzoaragaki@gmail.com>
2018-01-08 16:50:10 +0900

    会員登録機能追加

--- WEB-INF/src/api/CheckLogin.java
+++ WEB-INF/src/api/CheckLogin.java
@@ -1,12 +1,9 @@
 package api;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
-import actionform.Const;
-import actionform.Key;
 import actionform.form.LoginForm;
 import database.Query;
 import database.Sql;
@@ -24,323 +21,32 @@ public class CheckLogin {
 		// ① 氏名を受け取る
 		String name = loginForm.getName();
 		// ② ニックネームを受け取る
-		String nickname= loginForm.getName();
+		String nickname= loginForm.getNickname();
 		// ③ここで メールアドレスを受け取る
 		String mail = loginForm.getMail();
 		// ④ここで 住所を受け取る
 		String address = loginForm.getAddress();
 		// ⑤ここで 年齢を受け取る
-//		Int 年齢 = loginForm.get年齢();
 		String age = loginForm.getAge();
 
-		String sql = Sql.GET_TOROKU_NAME_SQL;
-		sql = sql.replaceFirst("name", name);
-		sql = sql.replace("nickname", nickname);
-		sql = sql.replace("mail", mail);
-		sql = sql.replace("address", address);
-		sql = sql.replace("age", age);
+		String sql = Sql.INSERT_TOROKU_SQL;
+
+		sql = sql.replaceFirst("nameVal", name);
+		sql = sql.replaceFirst("nicknameVal", nickname);
+		sql = sql.replaceFirst("mailVal", mail);
+		sql = sql.replaceFirst("addressVal", address);
+		sql = sql.replaceFirst("ageVal", age);
+
 		Map<String, String> containerMap = new HashMap<String, String>();
 		try {
 			Query query = new Query();
-			ResultSet result = query.getSelectResult(sql);
 
+		int aaa = 0;;
+		int i =aaa; query.getInsertResult(sql);
 
-			if(result.next()) {
-				containerMap.put(Key.SHORI_KEKKA, Const.SHORI_KEKKA_OK);
-
-				containerMap.put(Key.name, result.getString("name"));
-				containerMap.put(Key.nickname, result.getString("nickname"));
-				containerMap.put(Key.mail, result.getString("mail"));
-				containerMap.put(Key.address, result.getString("address"));
-				containerMap.put(Key.age, result.getString("age"));
-			} else {
-				containerMap.put(Key.SHORI_KEKKA, Const.SHORI_KEKKA_NG);
-			}
 		} catch (SQLException e) {
 			throw new SQLException(e);
 		}
 		return containerMap;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//package api;
-//
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import actionform.Const;
-//import actionform.Key;
-//import actionform.form.LoginForm;
-//import database.Query;
-//import database.Sql;
-//
-//
-//public class CheckLogin {
-//
-//	// コンストラクタ
-//	public CheckLogin () {
-//
-//	}
-//
-//	public Map<String, String> checkLogin (LoginForm loginForm) throws SQLException {
-//
-//		// ① loginIdを受け取る
-//		String loginId = loginForm.getLoginId();
-//		// ② loginPasswordを受け取る
-//		String loginPassword = loginForm.getLoginPassword();
-//		// ③ここで loginTestを受け取る
-//		String comment = loginForm.getComment();
-//		// ④ここで commentを受け取る
-//		String mailaddress = loginForm.getmailaddress();
-//		// ⑤ここで mailaddressを受け取る
-//
-//		String sql = Sql.GET_USER_INFO_NAME_SQL;
-//		sql = sql.replaceFirst("userId", loginId);
-//		sql = sql.replace("loginPassword", loginPassword);
-//		Map<String, String> containerMap = new HashMap<String, String>();
-//		try {
-//			Query query = new Query();
-//			ResultSet result = query.getSelectResult(sql);
-//
-//
-//
-//			if(result.next()) {
-//				containerMap.put(Key.SHORI_KEKKA, Const.SHORI_KEKKA_OK);
-//				containerMap.put(Key.NAME_SEI, result.getString("NAME_SEI"));
-//				containerMap.put(Key.NAME_MEI, result.getString("NAME_MEI"));
-//			} else {
-//				containerMap.put(Key.SHORI_KEKKA, Const.SHORI_KEKKA_NG);
-//			}
-//		} catch (SQLException e) {
-//			throw new SQLException(e);
-//		}
-//		return containerMap;
-//	}
-//}
-//
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//package api;
-//
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import actionform.Const;
-//import actionform.Key;
-//import actionform.form.LoginForm;
-//import database.Query;
-//import database.Sql;
-//
-//
-//public class CheckLogin {
-//
-//	// コンストラクタ
-//	public CheckLogin () {
-//
-//	}
-//
-//	public Map<String, String> checkLogin (LoginForm loginForm) throws SQLException {
-//
-//		// ① loginIdを受け取る
-//		String loginId = loginForm.getLoginId();
-//		// ② loginPasswordを受け取る
-//		String loginPassword = loginForm.getLoginPassword();
-//		// ③ここで loginTestを受け取る
-//		String comment = loginForm.getComment();
-//		// ④ここで commentを受け取る
-//		String mailaddress = loginForm.getmailaddress();
-//		// ⑤ここで mailaddressを受け取る
-//
-//		String sql = Sql.GET_USER_INFO_NAME_SQL;
-//		sql = sql.replaceFirst("userId", loginId);
-//		sql = sql.replace("loginPassword", loginPassword);
-//		sql = sql.replace("nameMei", mailaddress);
-//		Map<String, String> containerMap = new HashMap<String, String>();
-//		try {
-//			Query query = new Query();
-//			ResultSet result = query.getSelectResult(sql);
-//
-//
-//			if(result.next()) {
-//				containerMap.put(Key.SHORI_KEKKA, Const.SHORI_KEKKA_OK);
-//
-//				containerMap.put(Key.USER_ID, result.getString("USER_ID"));
-//				containerMap.put(Key.PASSWORD, result.getString("PASSWORD"));
-//				containerMap.put(Key.NAME_SEI, result.getString("NAME_SEI"));
-//				containerMap.put(Key.NAME_MEI, result.getString("NAME_MEI"));
-//			} else {
-//				containerMap.put(Key.SHORI_KEKKA, Const.SHORI_KEKKA_NG);
-//			}
-//		} catch (SQLException e) {
-//			throw new SQLException(e);
-//		}
-//		return containerMap;
-//	}
-//}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-////package api;
-////
-////import java.sql.ResultSet;
-////import java.sql.SQLException;
-////import java.util.HashMap;
-////import java.util.Map;
-////
-////import actionform.Const;
-////import actionform.Key;
-////import actionform.form.LoginForm;
-////import database.Query;
-////import database.Sql;
-////
-////
-////public class CheckLogin {
-////
-////	// コンストラクタ
-////	public CheckLogin () {
-////
-////	}
-////
-////	public Map<String, String> checkLogin (LoginForm loginForm) throws SQLException {
-////
-////		// ① loginIdを受け取る
-////		String loginId = loginForm.getLoginId();
-////		// ② loginPasswordを受け取る
-////		String loginPassword = loginForm.getLoginPassword();
-////		// ③ここで loginTestを受け取る
-////		String comment = loginForm.getComment();
-////		// ④ここで commentを受け取る
-////		String mailaddress = loginForm.getmailaddress();
-////		// ⑤ここで mailaddressを受け取る
-////
-////		String sql = Sql.GET_USER_INFO_NAME_SQL;
-////		sql = sql.replaceFirst("userId", loginId);
-////		sql = sql.replace("loginPassword", loginPassword);
-////		Map<String, String> containerMap = new HashMap<String, String>();
-////		try {
-////			Query query = new Query();
-////			ResultSet result = query.getSelectResult(sql);
-////
-////
-////
-////			if(result.next()) {
-////				containerMap.put(Key.SHORI_KEKKA, Const.SHORI_KEKKA_OK);
-////				containerMap.put(Key.NAME_SEI, result.getString("NAME_SEI"));
-////				containerMap.put(Key.NAME_MEI, result.getString("NAME_MEI"));
-////			} else {
-////				containerMap.put(Key.SHORI_KEKKA, Const.SHORI_KEKKA_NG);
-////			}
-////		} catch (SQLException e) {
-////			throw new SQLException(e);
-////		}
-////		return containerMap;
-////	}
-////}
-////
-////
-
-
-
-
-//--------------------------------------------------------------------------------------1224
-//package api;
-//
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import actionform.Const;
-//import actionform.Key;
-//import actionform.form.LoginForm;
-//import database.Query;
-//import database.Sql;
-//
-//
-//public class CheckLogin {
-//
-//	// コンストラクタ
-//	public CheckLogin () {
-//
-//	}
-//
-//	public Map<String, String> checkLogin (LoginForm loginForm) throws SQLException {
-//
-//		String loginId = loginForm.getLoginId();
-//		String loginPassword = loginForm.getLoginPassword();
-//		String sql = Sql.GET_USER_INFO_NAME_SQL;
-//		sql = sql.replaceFirst("userId", loginId);
-//		sql = sql.replace("loginPassword", loginPassword);
-//		Map<String, String> containerMap = new HashMap<String, String>();
-//		try {
-//			Query query = new Query();
-//			ResultSet result = query.getSelectResult(sql);
-//
-//			if(result.next()) {
-//				containerMap.put(Key.SHORI_KEKKA, Const.SHORI_KEKKA_OK);
-//				containerMap.put(Key.NAME_SEI, result.getString("NAME_SEI"));
-//				containerMap.put(Key.NAME_MEI, result.getString("NAME_MEI"));
-//			} else {
-//				containerMap.put(Key.SHORI_KEKKA, Const.SHORI_KEKKA_NG);
-//			}
-//		} catch (SQLException e) {
-//			throw new SQLException(e);
-//		}
-//		return containerMap;
-//	}
-//}
--- myPage.jsp
+++ myPage.jsp
@@ -20,7 +20,7 @@
 
 
 	    <tr>
-	      <td><font size="3">mai</font></td>
+	      <td><font size="3">mail</font></td>
 	      <td><bean:write name="loginForm" property="mail" /></td>
 	    </tr>
 	    <tr>
@@ -40,80 +40,3 @@
   </body>
 </html:html>
 
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- //--------------------------------------------------------------------------------------1224 -->
-<%-- <%@ page contentType="text/html;charset=UTF-8" %> --%>
-<%-- <%@ taglib uri="http://struts.apache.org/tags-html" prefix="html" %> --%>
-<%-- <%@ taglib uri="http://struts.apache.org/tags-bean" prefix="bean" %> --%>
-<%-- <html:html> --%>
-<!--   <head> -->
-<%--     <title><bean:message key="title.mypage"/></title> --%>
-<!--   </head> -->
-<!--   <body> -->
-<!--     <table> -->
-
-<!-- <h1>おめでとうございます。ログインが出来ました。</h1> -->
-<!-- 	    <tr> -->
-<!-- 	      <td><font size="3">USER_ID</font></td> -->
-<%-- 	      <td><bean:write name="loginForm" property="loginId" /></td> --%>
-<!-- 	    </tr> -->
-<!-- 	    <tr> -->
-<!-- 	      <td><font size="3">PASSWORD</font></td> -->
-<%-- 	      <td><bean:write name="loginForm" property="loginPassword" /></td> --%>
-<!-- 	    </tr> -->
-
-
-<!-- 	    <tr> -->
-<!-- 	      <td><font size="3">COMMENT</font></td> -->
-<%-- 	      <td><bean:write name="loginForm" property="nameSei" /></td> --%>
-<!-- 	    </tr> -->
-<!-- 	    <tr> -->
-<!-- 	      <td><font size="3">MAIL_ADDRESS</font></td> -->
-<%-- 	      <td><bean:write name="loginForm" property="nameMei" /></td> --%>
-<!-- 	    </tr> -->
-
-<!--     </table> -->
-
-<!-- <!-- 	   <br> <a href="/./login">トップへ</a> --> -->
-
-<!--   </body> -->
-<%-- </html:html> --%>
-
-
-
-
-
-
-
-
-<%-- <%@ page contentType="text/html;charset=UTF-8" %> --%>
-<%-- <%@ taglib uri="http://struts.apache.org/tags-html" prefix="html" %> --%>
-<%-- <%@ taglib uri="http://struts.apache.org/tags-bean" prefix="bean" %> --%>
-<%-- <html:html> --%>
-<!--   <head> -->
-<%--     <title><bean:message key="title.mypage"/></title> --%>
-<!--   </head> -->
-<!--   <body> -->
-<!--     <table> -->
-<!-- 	    <tr> -->
-<!-- 	      <td><font size="3">Id</font></td> -->
-<%-- 	      <td><bean:write name="loginForm" property="nameSei" /></td> --%>
-<!-- 	    </tr> -->
-<!-- 	    <tr> -->
-<!-- 	      <td><font size="3">Name</font></td> -->
-<%-- 	      <td><bean:write name="loginForm" property="nameMei" /></td> --%>
-<!-- 	    </tr> -->
-<!--     </table> -->
-<!--   </body> -->
-<%-- </html:html> --%>
